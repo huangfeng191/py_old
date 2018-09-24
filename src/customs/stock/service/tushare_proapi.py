cdecd27170f0442b8e35f61f564e1ce3f2b9908e
fengfeng <huangfeng191@163.com>
2018-09-24 22:21:35 +0800

    1

---
+++ src/customs/stock/service/tushare_proapi.py
@@ -0,0 +1,57 @@
+# -*- coding: UTF-8 -*-
+# Module  : py
+# Description :基本接口
+# Author  : Wujj
+# Date    : 2017-11-5
+# Version : 1.0
+
+
+
+import ctx
+from service import comm
+
+stock_interface_config = comm.CRUD(ctx.tusharedb, "stock_interface_config" )
+# 获取股票基本信息
+stock_basics = comm.CRUD(ctx.tusharedb, "stock_basics", [("code", 1)])
+# 行业分类
+industry_classified = comm.CRUD(ctx.tusharedb, "industry_classified", [("code", 1)])
+# 概念分类
+concept_classified = comm.CRUD(ctx.tusharedb, "concept_classified", [("code", 1)])
+
+# 地域分类
+area_classified = comm.CRUD(ctx.tusharedb, "area_classified", [("code", 1)])
+
+# 考虑用on_upsert
+#stock_basics.inject(industry_classified,"i_c","code",False,"code")
+
+import json
+import time
+
+import tushare as ts
+
+def getInfo(table_nm,method=None,icount=1):
+    if table_nm:
+        configRow=stock_interface_config.get({"table_nm":table_nm})
+    if method:
+        df=eval("ts.%s"%method)()
+    else:
+        df=eval("ts.get_%s"%table_nm)()
+    if configRow:
+        indexKey=  configRow.get("indexKey")
+    df_index=df.index
+    if not isinstance(df,list):
+        print "OK"
+        l=json.loads(df.to_json(orient='records'))
+        for i,r in enumerate(l):
+            if indexKey:
+               r[indexKey]=df_index[i]
+            eval(table_nm).upsert(**r)
+    else:
+    #     考虑写入错误日志 ，重新发起请求
+        time.sleep(10)
+        print "err"
+        if icount<5:
+            getInfo(table_nm,method,icount=icount+1)
+        else:
+            print "total_err"
+    return "OK"
---
+++ src/customs/stock/ui/stock_proui.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF-8 -*-
+# Description 查询数据
+from ui import path, CRUD, wildcard
+import web
+from web.contrib.template import render_mako
+from customs.stock.service.tushare_api import *
+render_stock = render_mako(directories=["customs/stock/templates", "templates"], input_encoding="utf-8",
+                       output_encoding="utf-8")
+
+from customs.stock.ui import bindinterfaceConfig
+
+
+@path("/stock/interfaceconfig.html")
+class StockInterfaceconfig:
+    def GET(self, _cid=None, *args, **kwargs):
+        return render_stock["interfaceconfig"]()
+
+
+@wildcard("/stock/interfaceconfig/")
+class StockInterfaceconfigCRUD(CRUD):
+    def __init__(self):
+        self.module = stock_interface_config
+    def query(self, count=True, *args, **kwArgs):
+
+       res = CRUD.query(self, count=count, *args, **kwArgs)
+
+       return res
+
+
+
+@path("/stock/interfacedata.html")
+class StockInterfaceData:
+    def GET(self, _cid = None, *args, **kwargs):
+        return render_stock["interfaceData"]()
+
+
+@wildcard("/stock/interfacedata/")
+class StockInterfaceDataCRUD(CRUD):
+
+    def __init__(self):
+        self.module = stock_interface_config
+    @bindinterfaceConfig
+    def action(self, act, *args, **kwArgs):
+         return CRUD.action(self, act, *args, **kwArgs)
+
+
+
+
+@path("/stock/bindings.js")
+class StockBindings:
+
+
+    def GET(self, _customer=None, _cid=None, _role=None, *args, **kwArgs):
+
+      web.header("Content-Type", "text/javascript", True)
+
+      #用户自定义数据字典
+      bindings, codeset = [], set([])
+      for r in stock_interface_config.items(query={'cid':_cid}):
+          if   'code' in r:
+              for rr in r.get("dtls"):
+                  rr["name"]=r.get("nm")
+                  rr["value"]=r.get("code")
+              bindings.append(
+        {"Code":r.get("code"),"Records":r.get("dtls",[])}
+      )
+      bindings.append(
+        {"Code":"STATE","Records":[
+          {"_id":"1","name":"启用", "value":"1"},
+          {"_id":"2","name":"停用", "value":"2"},
+        ]}
+      )
+      return "var GBindings = %s; " % json.dumps(bindings)
