23f36aa2550210c5e05dfae6b7e252e11ce30234
fengfeng <huangfeng191@163.com>
2018-09-26 22:19:49 +0800

    12

--- src/static/stock/JS/prolib.js
+++ src/static/stock/JS/prolib.js
@@ -44,82 +44,113 @@ function getBindConfig(){
 
 function getInterfaceConfig({table_nm}){
         var retO={
-            nm:"",
+            nm:"", // 记录名
             oOne:{},
             columns:[],
             inputs:[],
+            inputsRet:[[]],
             orders:[],
             quicks:[],
-            inputsRet:[[]]
+            props:[],
+          
         };
 
       var p= co($.po("/prostock/interfaceconfig/query.json",{ query: {'table_nm':table_nm} },{"async":false})).done(function(json)
       {
-          if(json.rows&&json.rows.length>0){
+        if(json.rows&&json.rows.length>0){
+            // colInp 列定义
             if(json.rows[0].colInp){
+                retO.nm=json.rows[0].nm;
+                // 以下为处理列
                 let tempStr=json.rows[0].colInp;
-                temp2a=tempStr.split("\n");
-                retO.nm=json.rows[0].nm
-                temp2a=temp2a.map(function(v){
-                    if(v.split(",").length>1){
-                        return v.split(",")
-                    }else{
-                        return v.split("：")
+                let temp2a=tempStr.split("\n");// 字段数组
+                    temp2a=temp2a.map(function(v){ //变为二维数组
+                        if(v.split(",").length>1){
+                            return v.split(",")
+                        }else{
+                            return v.split("：")
+                        }
+                    })
+                //  开始解析字段处理逻辑
+                //  数据格式：  sn ,dataType,nm,width
+                    temp2a.forEach(element => {
+                        var sn=element[0]||""
+                        var nm=element[2]||""
+                        var width=element[3]||"100";
+                        var dataType="String";
+                        if(element[1].includes["str","String","string"]){
+                            dataType="String";
+                        }
+                        var showType="text";
+                        var other={"columns":[],"inputs":[],"props":[],"quicks":[]};
+                        let oDdic=GetBindRow(sn,"Relation");
+                        if(oDdic){
+                            showType="combo"
+                            other["binding"]=oDdic.value;
+                            other["columns"]=[`,"binding": "${other["binding"]}"`]
+                            other["inputs"]=[`,"Ext": "${other["binding"]}"`]
+                            other["props"]=[`,"Ext": "${other["binding"]}"`]
+                            other["quicks"]=[`,"Ext": "${other["binding"]}"`,` "Source": "${other["binding"]}","TextField": "name", "ValueField": "value"`]
+                        }
+                      
+                        // 保存字段配置
+                        retO["oOne"][sn]={sn,dataType,nm,width,showType,other};
+
+                        var templateCols=`{ "field": "${sn}","title":"${nm}","width":"${width}", "halign": "center" ${other["columns"].join(",")} }`;
+                        var templateInputs=` { "Field": "${sn}", "Name": "${nm}", "ShowType":"${showType}" ${other["inputs"].join(",")} } `
+                        var templateProps=`{ "Field":"${sn}", "Name" : "${nm}", "ShowType":"${showType}","DataType": "${dataType}" ,"FilterEnabled": true,"OrderEnabled": true ${other["props"].join(",")}  }`;
+                        retO.columns.push(JSON.parse(templateCols))
+                        retO.inputs.push(JSON.parse(templateInputs))
+                        retO.props.push(JSON.parse(templateProps))
+                    })
+
+
+                    if(json.rows[0].orders){
+                        let ordersStr=json.rows[0].orders;
+                        aOrdersStr=ordersStr.split(",")
+                        aOrdersStr.forEach(element => {
+                            let sn=element||""
+                            var templateOrder=`
+                            { "Field": "${sn}", "Type": false }
+                           `
+                           retO.orders.push(JSON.parse(templateOrder))
+                        });
+                       
+                    };
+                    if(json.rows[0].quicks){
+                        let quicksStr=json.rows[0].quicks;
+                        aQuicksStr=quicksStr.split("\n")
+                        aQuicksStr.forEach(element => {
+                            let sn=element.split(",")[0]||"";
+                           if(retO["oOne"][sn]){
+                               let nm=retO["oOne"][sn].nm;
+                               let width=retO["oOne"][sn].width;
+                               let dataType=retO["oOne"][sn].dataType;
+                               let showType=retO["oOne"][sn].showType;
+                               let type=(showType=="combo"?"QCombox":"QText");
+                              
+                               let other=retO["oOne"][sn].other;
+                           
+                            var templateQuicks=` { "Field": "${sn}", "Label": "${nm}",  "Type": "${type}", "Width": ${width} ${other["quicks"].join(",")} } `;
+                           
+                            
+                            retO.quicks.push(JSON.parse(templateQuicks))
+                           
+                           }
+                           
+                        });
+                       
                     }
-                })
-                temp2a.forEach(element => {
-                    var sn=element[0]||""
-                    // var tp=element[1]||""; 数据类型，暂时不用
-                    var nm=element[2]||""
-                    var width=element[3]||"100"
-                    retO["oOne"][sn]={sn,nm};
-                    
-                    let oDdic=GetBindRow(sn,"Relation");
-                    var templateCols=`
-                        { "field": "${sn}","title":"${nm}","width": `+width+`, "halign": "center"`+
-                        ( oDdic?(`,"binding":"`+oDdic.value+`"`):"")+
-                        `}
-                    `
-                    var templateInputs=`
-                    { "Field": "${sn}", "Name": "${nm}" }
-                    `
-                    retO.columns.push(JSON.parse(templateCols))
-                    retO.inputs.push(JSON.parse(templateInputs))
-                });
+
+
+
+
+
             }
-            if(json.rows[0].orders){
-                let ordersStr=json.rows[0].orders;
-                aOrdersStr=ordersStr.split(",")
-                aOrdersStr.forEach(element => {
-                    let sn=element||""
-                    var templateOrder=`
-                    { "Field": "${sn}", "Type": false }
-                   `
-                   retO.orders.push(JSON.parse(templateOrder))
-                });
-               
-            };
-            if(json.rows[0].quicks){
-                let quicksStr=json.rows[0].quicks;
-                aQuicksStr=quicksStr.split("\n")
-                aQuicksStr.forEach(element => {
-                    debugger
-                    let sn=element.split(",")[0]||"";
-                    
-                 
-                   if(retO["oOne"][sn]){
-                       let nm=retO["oOne"][sn].nm
-                       var templateQuicks=`
-                       { "Field": "${sn}", "Label": "${nm}",  "Type"   : "QText", "Width": 70 }
-                      `
-                    retO.quicks.push(JSON.parse(templateQuicks))
-                   }
-                   
-                });
-               
-            }
-          }
-      })
+        }
+
+    })
+    //  input 处理分行
       let oneRow=[]
       retO.inputs.forEach(function(v,i){
         oneRow.push(v);
